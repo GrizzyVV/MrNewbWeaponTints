34ebf2ea5696b0ff8c122f5b51f840502e3afdce
GrizzyVV <84356682+GrizzyVV@users.noreply.github.com>
2023-10-19 01:24:13 -0400

    Update Config.lua
    - Added necessary table structure for easy of config.lua
    *newbserialfile* is hard-coded in server.lua back end and may need to be refactored to be more accommodating for different item names.

--- Config.lua
+++ Config.lua
@@ -4,3 +4,48 @@ Config.scratchedserial = "The serial has been scratched off."  -- Can place what
 Config.breakpoint = 9  --- number to hit from breakpoint greater than or equal to chancerandom
 Config.chancerandom = math.random(1,10)	--- math random min, max numbers to roll for break chance
 Config.debugprints = true	-- do you want strings printing in the console to debug?
+
+
+Config.Itemslist = {
+    ['newbserialfile'] = { -- using the key as the export name and the item name to make things easier.
+        item = 'newbserialfile',
+        tintvalue = nil,
+        tintname = nil
+    },
+    ['newbtint1'] = {
+        item = 'newbtint1',
+        tintvalue = 1,
+        tintname = 'Green Weapon Tint',
+    },
+    ['newbtint2'] = {
+        item = 'newbtint2',
+        tintvalue = 2,
+        tintname = 'Gold Weapon Tint',
+    },
+    ['newbtint3'] = {
+        item = 'newbtint3',
+        tintvalue = 3,
+        tintname = 'Pink Weapon Tint',
+    },
+    ['newbtint4'] = {
+        item = 'newbtint4',
+        tintvalue = 4,
+        tintname = 'Army Weapon Tint',
+    },
+    ['newbtint5'] = {
+        item = 'newbtint5',
+        tintvalue = 5,
+        tintname = 'LSPD Weapon Tint',
+    },
+    ['newbtint6'] = {
+        item = 'newbtint6',
+        tintvalue = 6,
+        tintname = 'Orange Weapon Tint',
+    },
+    ['newbtint7'] = {
+        item = 'newbtint7',
+        tintvalue = 7,
+        tintname = 'Platinum Weapon Tint',
+    },
+
+}
